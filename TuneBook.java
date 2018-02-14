42ebd0689f8907749ff3e6d16d22860e6b5a9a3b
Ferd1nat0r <31894646+Ferd1nat0r@users.noreply.github.com>
2018-02-14 17:25:49 +0000

    Add files via upload

--- TuneBook.java
+++ TuneBook.java
@@ -1,10 +1,38 @@
 package ie.dit;
-
+import java.io.IOException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.io.BufferedReader;
 
 public class TuneBook
 {
 	ArrayList<String> tunes = new ArrayList<String>();
 	
-	
+	BufferedReader inputStream<String> = null;
+	try {
+		inputStream<String> = new BufferedReader(new FileReader("hmj0.abc"));
+    
+		String l;
+		while ((l = inputStream.readLine()) != null) 
+		{
+			System.out.println(l);
+		}
+	}
+	catch (IOException e)
+	{
+		e.printStackTrace();
+	} 
+	finally 
+	{
+		if (inputStream != null) {
+			try
+			{
+				inputStream.close();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}    
 }
